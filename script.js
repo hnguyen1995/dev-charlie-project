298fcf2352f806403ee0174d094614e8fbc29514
hnguyen1995 <hnguyen1@una.edu>
2025-02-24 16:23:42 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  console.log("Welcome to the Past Projects Dashboard!");
+
+  // Easter Egg: Secret message in console
+  console.log("Easter Egg: You found the secret message! 🎉");
+});
